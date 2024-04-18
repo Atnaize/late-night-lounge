d97665c158bc71a97c1915ba572e469fca9302b4
Eric Debleumortier <eric.debleumortier@3dside.eu>
2024-04-18 17:52:30 +0200

    Change color

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,21 @@ import { TypeAnimation } from 'react-type-animation';
 export default function Home() {
   return (
     <main className="flex max-h-screen flex-col items-center justify-between p-24">
-      <div className="text-6xl font-bold text-gray-400 tracking-wide leading-tight">Late Night Lounge</div>
-      <TypeAnimation
-        sequence={[
-          "Elegance in Every Sip",
-          1000,
-          "Laughter on Every Lip",
-          1000,
-        ]}
-        speed={20}
-        repeat={Infinity}
-      />
+      <div className="text-6xl text-gray-400 font-sans font-light underline underline-offset-8 pb-5">Late Night Lounge</div>
+      <div className="">
+        <TypeAnimation
+          sequence={[
+            "Elegance in Every Sip",
+            1000,
+            "Laughter on Every Lip",
+            1000,
+          ]}
+          speed={20}
+          repeat={Infinity}
+          className="text-grey-600"
+        />
+      </div>
+
       <div className="">
         <Image
           className="relative dark:invert"
