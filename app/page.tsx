106e74dc75d9ac9f48f6b107c335ab7e65208431
Eric Debleumortier <eric.debleumortier@3dside.eu>
2024-04-18 13:11:28 +0200

    Fix build

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { TypeAnimation } from 'react-type-animation';
 
-export default function Home(Elem: React.ComponentClass<any>) {
+export default function Home() {
   return (
     <main className="flex max-h-screen flex-col items-center justify-between p-24">
       <div className="text-6xl font-bold text-gray-400 tracking-wide leading-tight">Late Night Lounge</div>
